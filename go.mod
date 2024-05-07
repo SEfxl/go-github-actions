3586c4890ea51772ac68daf1f8797aa98fb7a3b0
SEfxl <17862809558@163.com>
2024-05-07 18:13:56 +0800

    新增action

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SEfxl/go-github-actions
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/SEfxl/go-github-actions/hello"
+)
+
+func main() {
+	fmt.Println(hello.Greet())
+}
